13db59c3d4f7c4b9016cd537399a46651b68d705
junioralbino741@gmail.com <34866064+junioralbino@users.noreply.github.com>
2020-09-08 19:15:05 -0300

    Add files via upload

---
+++ Selecionar de data/script.js
@@ -0,0 +1,34 @@
+$(function(){
+	var horaescolhacampo;
+
+	$('input').bind('focus', function(){
+
+		var pos = $(this).offset();
+		var width = $(this).width();
+
+		$('.horaescolha').css('left', pos.left + width + 5);
+		$('.horaescolha').css('top', pos.top);
+		$('.horaescolha').show();
+
+      horaescolhacampo = $(this);
+
+	});
+
+	$('input').bind('blur', function(){
+
+		setTimeout(function(){
+		   $('.horaescolha').hide();	
+
+		},200);
+    
+	});
+
+
+	$('.horaescolha button').bind('click', function(){
+
+		var hora = $(this).html();
+		  horaescolhacampo.val(hora);
+
+	});
+
+});
